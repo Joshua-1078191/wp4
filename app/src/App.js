d640ab7a77b18a1450c004638f5e81f82d7b5845
Slava 2400758 <2400758@hr.nl>
2025-06-19 01:30:35 +0200

    Bron en brontoevoegen aan app.js toegevoegd

--- app/src/App.js
+++ app/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Home from './pages/Home';
+import Bronnen from './pages/Bronnen';
+import BronToevoegen from './pages/BronToevoegen';
 import './App.css';
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/bronnen" element={<Bronnen />} />
+          <Route path="/bron-toevoegen" element={<BronToevoegen />} />
           <Route path="/" element={<Navigate to="/login" replace />} />
         </Routes>
       </div>
