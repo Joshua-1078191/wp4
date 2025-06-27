e7e10802e0a542fa9410cdff60a782b2e152d495
Slava 2400758 <2400758@hr.nl>
2025-06-27 21:59:32 +0200

    Zoek werkte alleen door double click. weggehaald

--- app/src/pages/Bronnen.js
+++ app/src/pages/Bronnen.js
@@ -190,8 +190,6 @@ function Bronnen() {
                 setZoekTerm(pendingZoekTerm);
                 setTypeFilter(pendingTypeFilter);
                 setCategorieFilter(pendingCategorieFilter);
-                setLoading(true);
-                setTimeout(haalBronnenOp, 0);
               }}
             >
               Zoek
